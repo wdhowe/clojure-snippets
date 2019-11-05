37b068278d93fd06fcde4d571da3e3c6a608bcbb
whowe <howe.bill@gmail.com>
2019-11-04 21:49:18 -0600

    Clojure hello world.

---
+++ hello_world.clj
@@ -0,0 +1,11 @@
+;; Clojure namespace
+(ns clojure.examples.hello
+  (:gen-class))
+
+;; Clojure function
+(defn hello-world []
+  (println "Hello Clojure world!"))
+
+;; Calling a function
+(hello-world)
+
