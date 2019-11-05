7a900c10c035c66b904740c2d6f28bd96f2c8bee
whowe <howe.bill@gmail.com>
2019-11-04 21:55:47 -0600

    Check for a file.

---
+++ file_check.clj
@@ -0,0 +1,12 @@
+(ns file_check
+(:gen-class)
+(:require [clojure.java.io :as io])
+)
+
+(defn main []
+  (println "Checking for file 'myfile.txt'")
+  (println "Does file exist?:" (.exists (io/file "myfile.txt")))
+)
+
+(main)
+
