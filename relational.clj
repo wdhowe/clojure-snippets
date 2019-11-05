2240105f3ac459e9364934465a1a9bae28a29fbb
whowe <howe.bill@gmail.com>
2019-11-04 21:54:49 -0600

    Relational comparisons.

---
+++ relational.clj
@@ -0,0 +1,15 @@
+(ns clojure.examples.relational
+  (:gen-class))
+
+(defn do-compare []
+  (println "Equality:" (= 7 7))
+  (println "Not Equality:" (not= 7 7))
+  (println "Less than:" (< 7 8))
+  (println "Greater than:" (> 7 8))
+  (println "And op:" (and true false))
+  (println "Or op:" (or true false))
+  (println "Not op:" (not false))
+)
+
+(do-compare)
+
