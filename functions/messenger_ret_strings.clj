f785ed16e93ff2323c826425d3975e8bf7838bbb
whowe <howe.bill@gmail.com>
2019-12-15 22:59:18 -0600

    Messenger version that returns strings instead of printing. Also with assert tests.

---
+++ functions/messenger_ret_strings.clj
@@ -0,0 +1,22 @@
+; function that can handle zero, 1 or two args
+(defn messenger
+  ([] (messenger "world!"))
+  ([msg] (str "Hello, " msg))
+  ([msg1 msg2] (str msg1 msg2))
+)
+
+; call with no args
+(println (messenger) )
+
+; call with an argument
+(println (messenger "Bob") )
+
+; call with two arguments
+(println (messenger "the" "Bob") )
+
+;; testing with assert
+(println "Assertion tests below (nil is good/no error)")
+(println (assert (= "Hello, world!" (messenger))) )
+(println (assert (= "Hello, Bob" (messenger "Bob"))) )
+(println (assert (= "the Bob" (messenger "the" " Bob"))) )
+
