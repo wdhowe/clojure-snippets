577fcf41969834cbd517bcc955bff2c0538a4c74
whowe <howe.bill@gmail.com>
2019-12-15 20:35:50 -0600

    Clojure REPL helper functions

---
+++ repl_help.clj
@@ -0,0 +1,18 @@
+; Load clojure REPL helper functions
+; (require '[clojure.repl :refer :all])
+
+; doc - display documentation for a function
+; (doc +)
+
+; apropos - find function names that match a string
+; (apropos "+")
+
+; find-doc - wider function search that includes docstrings
+; (find-doc "trim")
+
+; dir - view full listing of functions in a namespace
+; (dir clojure.repl)
+
+; source - view function description and source
+; (source dir)
+
