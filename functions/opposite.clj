5759753993ea2342ea1cbfdd9722e6260a7339e0
whowe <howe.bill@gmail.com>
2019-12-16 21:21:36 -0600

    Negate a passed in function

---
+++ functions/opposite.clj
@@ -0,0 +1,13 @@
+; return a function that negates the passed in function
+(defn opposite [f]
+  (fn [& any] (not (apply f any)))
+)
+
+; true if both x and y are smaller than 100
+(defn smaller [x y]
+ (and (< x 100) (< y 100))
+)
+
+(def notsmaller (opposite smaller))
+
+(println (notsmaller 1000 80) )
