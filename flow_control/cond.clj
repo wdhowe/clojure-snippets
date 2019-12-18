fa4359aca0665ad85ddf5c0751156a0e32829395
whowe <howe.bill@gmail.com>
2019-12-17 22:57:46 -0600

    Conditional expression.

---
+++ flow_control/cond.clj
@@ -0,0 +1,10 @@
+;;; cond - series of conditional tests
+
+(def mynum 30)
+
+; return the first matched condition
+(cond
+      (< mynum 50) (println "Less than 50!")
+      (< mynum 60) (println "Less than 60!")
+      :else (println "Larger than 60!") )
+
