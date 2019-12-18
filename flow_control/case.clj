e2df09f964892239d87a48ff81b1a22d880d4e6b
whowe <howe.bill@gmail.com>
2019-12-17 23:10:23 -0600

    case expression with else.

---
+++ flow_control/case.clj
@@ -0,0 +1,11 @@
+;;; case expression
+
+(defn myfunc [x]
+  (case x
+    10 "x is 10!"
+    20 "x is 20!"
+    "x wasn't matched."))
+
+(println "Checking 20:" (myfunc 20))
+(println "Checking 30:" (myfunc 30))
+
