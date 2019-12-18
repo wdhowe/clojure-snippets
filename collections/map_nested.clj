7a324b12511067d3d9b319648ce9729372daa17f
whowe <howe.bill@gmail.com>
2019-12-17 20:31:03 -0600

    Nested maps.

---
+++ collections/map_nested.clj
@@ -0,0 +1,16 @@
+; nested maps
+
+(def person
+  {:name "The Dude"
+   :address {:street "123 Chill Ave"
+             :city "Malibu"
+             :state "California"}
+   }
+)
+
+(println "The nested map:" person)
+(println "City:" (get-in person [:address :city]) )
+
+; modifying nested maps
+(println "\nModifying the nested map:" (assoc-in person [:address :city] "San Diego") )
+
