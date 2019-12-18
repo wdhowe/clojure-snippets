18b8b67a793d1bd65ea95fdf7e36ec8a44645be7
whowe <howe.bill@gmail.com>
2019-12-17 18:47:08 -0600

    Show that different collection types can be joined together.

--- collections/sets.clj
+++ collections/sets.clj
@@ -9,7 +9,13 @@
 (println "\nIs Luke a Jedi?" (contains? jedi "luke"))
 (println "Sorted" (conj (sorted-set) "yoda" "obi-wan" "luke") )
 
+; sets can be joined with 'into'
 (def sith #{"vader", "palpatine"})
 
-(println "\nUsers of the force:" (into jedi sith))
+(println "\nUsers of the force (two sets):" (into jedi sith))
+
+; into can also join other collections (vectors, sets, lists, maps)
+(def friends (vector "joe" "john") )
+
+(println "\nUsers of the force and some friends (set and vector):" (into jedi friends))
 
