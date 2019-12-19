788a9c7ad801edc9a3b45f053b6a893d8468bd31
whowe <howe.bill@gmail.com>
2019-12-18 19:22:48 -0600

    Exception handling

---
+++ flow_control/exceptions.clj
@@ -0,0 +1,21 @@
+; Exception handling
+
+(def x 10)
+(def y 0)
+
+; try/catch/finally similar to Java
+(try
+     (/ x y)
+     (catch ArithmeticException e
+       "divide by zero")
+     (finally
+       (println "cleanup")
+     )
+)
+
+; throwing exceptions
+(try
+     (throw (Exception. "something bad happened"))
+     (catch Exception e (.getMessage e))
+)
+
