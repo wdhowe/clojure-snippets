7688cb3e9f37e6240c9ad6b16600620d7085cba6
whowe <howe.bill@gmail.com>
2020-01-01 15:33:04 -0600

    Additional list comprehension examples.

--- flow_control/for.clj
+++ flow_control/for.clj
@@ -1,5 +1,5 @@
-;; Clojure's for is a list comprehension, NOT a for loop
-; behaves like doseq
+;; Clojure's for is a list comprehension (Like Python's list comprehension)
+;; NOT a for loop. behaves like doseq
 
 ; sequential lists for below
 (def mylist '(5 "pizza" "something"))
@@ -13,3 +13,10 @@
 
 (println "New seq via for is:" newseq)
 
+(println "\nGenerated list of a range:"
+  (for [i (range 10)] i)
+)
+
+(println "\nGenerated odd list of a range:"
+  (for [i (range 10) :when (odd? i)] i)
+)
