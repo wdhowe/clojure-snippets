ff8ffc7bca4d17f039e196b75ff5ddaa7402d9fb
whowe <howe.bill@gmail.com>
2020-03-14 18:09:32 -0500

    date example

---
+++ date_display.clj
@@ -0,0 +1,10 @@
+;;;; Examples displaying date objects
+(ns date-display)
+
+(defn -main []
+  (let [date-year (.format (java.text.SimpleDateFormat. "yyyy-MM-dd") (new java.util.Date))
+        date-time (.format (java.text.SimpleDateFormat. "yyyy-MM-dd HH:mm") (new java.util.Date))]
+  (println "Date is: " date-year)
+  (println "Date with time is: " date-time)))
+
+(-main)
