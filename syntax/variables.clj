a98480bbf2d306e0968101792ac14f402fd94cc0
whowe <howe.bill@gmail.com>
2020-03-15 12:38:00 -0500

    Use let for local main variables instead of def.

--- syntax/variables.clj
+++ syntax/variables.clj
@@ -2,15 +2,15 @@
 
 (defn main
   []
-  (def myname "Robert")
-  (println "My name is:" myname)
-  (def myrate 50.32)
-  (println "My hourly rate is:" myrate)
-  (def myvar 100)
-  (println "My var is:" myvar)
-  (def myatomvar (atom 1))
-  (println "My atom var is:" @myatomvar)
-  (swap! myatomvar inc)
-  (println "My atom var is now:" @myatomvar))
+  (let [myname "Robert"
+        myrate 50.32
+        myvar 100
+        myatomvar (atom 1)]
+    (println "My name is:" myname)
+    (println "My hourly rate is:" myrate)
+    (println "My var is:" myvar)
+    (println "My atom var is:" @myatomvar)
+    (swap! myatomvar inc)
+    (println "My atom var is now:" @myatomvar)))
 
 (main)
