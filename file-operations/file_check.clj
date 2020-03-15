6550de78e7cbcc31c76d67eade46cc312223d832
whowe <howe.bill@gmail.com>
2020-03-15 11:19:02 -0500

    Formatted with lein zprint.

--- file-operations/file_check.clj
+++ file-operations/file_check.clj
@@ -1,12 +1,10 @@
 (ns file_check
-(:gen-class)
-(:require [clojure.java.io :as io])
-)
+  (:gen-class)
+  (:require [clojure.java.io :as io]))
 
-(defn main []
+(defn main
+  []
   (println "Checking for file 'myfile.txt'")
-  (println "Does file exist?:" (.exists (io/file "myfile.txt")))
-)
+  (println "Does file exist?:" (.exists (io/file "myfile.txt"))))
 
 (main)
-
--- file-operations/file_write.clj
+++ file-operations/file_write.clj
@@ -1,5 +1,2 @@
 ; with-open uses exception handling and auto closes files
-(with-open [f (clojure.java.io/writer "testfile.txt")]
-  (.write f "test text")
-)
-
+(with-open [f (clojure.java.io/writer "testfile.txt")] (.write f "test text"))
