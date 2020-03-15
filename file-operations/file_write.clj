29599e4a8366a972ba024ef000195fb5f233e586
whowe <howe.bill@gmail.com>
2020-03-15 12:00:36 -0500

    Added namespace and java io requirement.

--- file-operations/file_write.clj
+++ file-operations/file_write.clj
@@ -1,2 +1,6 @@
+(ns file-write
+  (:gen-class)
+  (:require [clojure.java.io :as io]))
+
 ; with-open uses exception handling and auto closes files
-(with-open [f (clojure.java.io/writer "testfile.txt")] (.write f "test text"))
+(with-open [f (io/writer "testfile.txt")] (.write f "test text"))
