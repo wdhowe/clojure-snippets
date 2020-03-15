a41e24d0d41c6559565b67abaf9d7f6e86e3cde7
whowe <howe.bill@gmail.com>
2020-03-15 11:15:29 -0500

    Formatted with lein zprint.

--- date_display.clj
+++ date_display.clj
@@ -1,10 +1,13 @@
 ;;;; Examples displaying date objects
 (ns date-display)
 
-(defn -main []
-  (let [date-year (.format (java.text.SimpleDateFormat. "yyyy-MM-dd") (new java.util.Date))
-        date-time (.format (java.text.SimpleDateFormat. "yyyy-MM-dd HH:mm") (new java.util.Date))]
-  (println "Date is: " date-year)
-  (println "Date with time is: " date-time)))
+(defn -main
+  []
+  (let [date-year (.format (java.text.SimpleDateFormat. "yyyy-MM-dd")
+                           (new java.util.Date))
+        date-time (.format (java.text.SimpleDateFormat. "yyyy-MM-dd HH:mm")
+                           (new java.util.Date))]
+    (println "Date is: " date-year)
+    (println "Date with time is: " date-time)))
 
 (-main)
