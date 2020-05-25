c305a30ebc64aba245a0a1437ed1ffa78c535000
wdhowe <howe.bill@gmail.com>
2020-05-25 13:58:34 -0500

    Simplified the map data structure example into the commonly used hash map.

--- data_structures/maps.clj
+++ data_structures/maps.clj
@@ -1,23 +1,32 @@
-; maps are used for key/value lookups or representing domain application data
-; key/value lookups = hash lookups/dictionaries in other languages
+;; maps - key/value lookups (dictionaries or hashes)
+;; Use when: You have natural key/value lookup requirements or
+;;           as a useful top level structure for nested data.
 
-(def lightsabers {"yoda" "green", "luke" "blue", "vader" "red"})
+; Creating maps - literal and keyword
+(def jedi1
+  {:name "Yoda", :lightsaber "green", :height "very small", :job "Jedi Sage", :foods {:favorite "pizza" :dislikes "spinach"}})
+(def jedi2
+  (hash-map :name "Luke" :lightsaber "blue" :height "normal" :job "Jedi Master" :foods {:favorite "burgers" :dislikes "fish"}))
 
-; displaying from the map
-(println "All lightsabers:" lightsabers)
-(println "Lightsaber for yoda:" (get lightsabers "yoda"))
-(println "Lightsaber for vader" (get lightsabers "vader"))
+; The maps
+(println "Jedi 1:" jedi1)
+(println "Jedi 2:" jedi2)
 
-; adding and removing from the map
-(println "\nAdding mace windu:" (assoc lightsabers "mace" "purple"))
-(println "Removing luke:" (dissoc lightsabers "luke"))
+; Getting items - get and keyword
+(println "\nLightsaber for yoda:" (get jedi1 :lightsaber))
+(println "Lightsaber for luke:" (:lightsaber jedi2))
+(println "Nested get for yoda (fav food):" (get-in jedi1 [:foods :favorite]))
 
-; checking map contents
-(println "\nDoes the map contain luke?" (contains? lightsabers "luke"))
-(println "Find vader:" (find lightsabers "vader"))
-(println "Bad lookup, use a default if not found:"
-         (get lightsabers "nobody" "none found"))
+; Adding/removing
+(println "\nAdding jedi1's home:" (assoc jedi1 :home "a swamp"))
+(println "Removing jedi2's job:" (dissoc jedi2 :job))
 
-; just keys or values
-(println "\nDisplay keys only:" (keys lightsabers))
-(println "Display values only:" (vals lightsabers))
+; Checking the map
+(println "\nDoes the jedi1 map contain a lightsaber?" (contains? jedi1 :lightsaber))
+(println "Find a job in the jedi2 map:" (find jedi2 :job))
+(println "Bad lookup in the jedi1 map, use a default if not found:"
+         (get jedi1 :nothing "stop it now"))
+
+; Display just keys or values
+(println "\nDisplay jedi1 map keys only:" (keys jedi1))
+(println "Display jedi1 map values only:" (vals jedi1))
