03abda18c220d26f37b3f037395c28123bf80b39
wdhowe <howe.bill@gmail.com>
2020-05-28 19:42:56 -0500

    Renamed messenger to better reflect what it contains. Updated comments.

--- functions/multi_arity_tests.clj
+++ functions/multi_arity_tests.clj
@@ -1,3 +1,5 @@
+;; multi-arity - function that treats different num of arguments in unique ways
+
 ; function that can handle zero, 1 or two args
 (defn messenger
   ([] (messenger "world!"))
@@ -13,8 +15,8 @@
 ; call with two arguments
 (println (messenger "the" "Bob"))
 
-;; testing with assert
-(println "Assertion tests below (nil is good/no error)")
+;; running tests with assert
+(println "\nAssertion tests below (nil is good/no error)")
 (println (assert (= "Hello, world!" (messenger))))
 (println (assert (= "Hello, Bob" (messenger "Bob"))))
 (println (assert (= "the Bob" (messenger "the" " Bob"))))
