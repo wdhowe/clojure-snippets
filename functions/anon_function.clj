f42e50e1df2ca4fdb4de99dbd4c2fdae89c78fd2
wdhowe <howe.bill@gmail.com>
2020-05-30 11:13:11 -0500

    Moved shorthand anon function examples into anon_function.clj. Added more useful examples.

--- functions/anon_function.clj
+++ functions/anon_function.clj
@@ -1,2 +1,33 @@
-; an anonymous function (lambda) - not bound to a name
-((fn [message] (println message)) "Hello lambda!")
+;; Anonymous functions (Lambda)
+
+;; Normal form
+
+; simple example
+(println "Multiply Example:" ((fn [x] (* x 3)) 50))
+
+; Using anon fn as return values.
+(defn my-inc
+  "Custom incrementor."
+  [increase-by]
+  (fn [num] (+ num increase-by)))
+
+(def inc10 (my-inc 10))
+
+(println "\nStart with 20, then custom inc:" (inc10 20))
+
+;; Short form - nesting not allowed
+; % -> single parameter
+; %1, %2 -> multiple parameters
+; %& -> remaining parameters
+
+; same examples as above, but in shorthand form.
+(println "\nMultiply Example:" (#(* % 3) 50))
+
+(defn my-inc2
+  "Custom incrementor."
+  [increase-by]
+  #(+ %1 increase-by))
+
+(def inc20 (my-inc2 20))
+
+(println "\nStart with 20, then custom inc:" (inc20 20))
