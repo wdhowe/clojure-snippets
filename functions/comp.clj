9b785bd86489086fd50910c08275510ae21abfa4
wdhowe <howe.bill@gmail.com>
2020-06-27 21:10:48 -0500

    Added comp example with an anon function.

--- functions/comp.clj
+++ functions/comp.clj
@@ -7,7 +7,11 @@
 ; compose functions together
 (def wrap-it (comp wrap-in-exclam wrap-in-hash wrap-in-money))
 
-(println "Composed function:" (wrap-it "YO"))
+(println "Composed function:" (wrap-it "HELLO"))
 
 (println "Same thing, not composed:"
-         (wrap-in-exclam (wrap-in-hash (wrap-in-money "YO"))))
+         (wrap-in-exclam (wrap-in-hash (wrap-in-money "HELLO"))))
+
+; another example using an anon function for transforming a number
+(def change-num (comp inc #(* % 2)))
+(println "\nChange 2:" (change-num 2))
