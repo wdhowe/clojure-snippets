80b0341fea41a0e68da08699b3b52037d4071f66
wdhowe <wdhowe@protonmail.com>
2020-08-03 16:25:28 -0500

    Show the two idiomatic Clojure ways to define records.

--- abstractions/records.clj
+++ abstractions/records.clj
@@ -5,12 +5,14 @@
 ; define the record and its fields:
 (defrecord VendItem [name brand description])
 
-; create some records
+; create some records - show two ways to create (map-> and ->)
 (def my-tp (map->VendItem {:name "tp" :brand "Wipem" :description "Nothing wipes like Wipem!"}))
-(def my-meat (map->VendItem {:name "meat" :brand "Its Real" :description "Tastes like the real thing!"}))
+(def my-meat (->VendItem "meat" "Its Real" "Tastes like the real thing!"))
+
+; when creating with "map->", not all fields are required.
 (def my-bleach (map->VendItem {:name "bleach" :brand "Not My Eyes"}))
 
-(println "Some records. Notice all keys are present, defined or not.")
+(println "Some records. Notice all keys are present, defined or not. (when using map->)")
 (println "My tp is:" my-tp)
 (println "My meat is:" my-meat)
 (println "My bleach is:" my-bleach)
