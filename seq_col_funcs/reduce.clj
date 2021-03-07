0c3247ce99a563312b549d03f080b8cf449b541d
wdhowe <wdhowe@protonmail.com>
2021-03-06 20:15:59 -0600

    - Added .calva/ to gitignore. - Added reduce-kv example in seq_col_funcs/reduce.clj.

--- seq_col_funcs/reduce.clj
+++ seq_col_funcs/reduce.clj
@@ -5,19 +5,25 @@
 (def mylist [1 2 3 4 5])
 
 (println
-  "Reduce mylist by applying the '+' to each pair (accumulating results):"
-  (reduce + mylist))
+ "Reduce mylist by applying the '+' to each pair (accumulating results):\n"
+ (reduce + mylist))
 
-(println "\nInitial value of 10:" (reduce + 10 mylist))
-
-(println
-  "\nCreating a hash-map from a vector."
-  (reduce (fn [new-map x] (assoc new-map (keyword x) (rand-int 100)))
-    {}
-    ["first" "second" "third"]))
+(println "\nInitial value of 10:\n" (reduce + 10 mylist))
 
 (println
- "\nCreating a new hash-map with updated values from an existing map."
+ "\nCreating a hash-map from a vector.\n"
+ (reduce (fn [new-map x] (assoc new-map (keyword x) (rand-int 100)))
+         {}
+         ["first" "second" "third"]))
+
+(println
+ "\nCreating a new hash-map with updated values from an existing map.\n"
  (reduce (fn [new-map [key value]] (assoc new-map key (inc value)))
          {}
          {:first 1 :second 2 :third 3}))
+
+(println
+ "\nCreating a new hash-map with updated values from an existing map, using reduce-kv instead of reduce.\n"
+ (reduce-kv (fn [m k v] (assoc m k (inc v)))
+            {}
+            {:first 1 :second 2 :third 3}))
